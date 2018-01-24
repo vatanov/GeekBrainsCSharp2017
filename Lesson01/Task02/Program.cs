d3764af0db9c10c098dd40f1274ac313643b46d2
Vladimir Atanov <atanov.v@gmail.com>
2018-01-24 21:26:51 +0200

    Lesson 1 Task 2 is done.

---
+++ Lesson01/Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your height in meters: ");
+            string h = Console.ReadLine();
+            double height;
+            double.TryParse(h, out height);
+
+            Console.Write("Enter your weight in kilos: ");
+            string w = Console.ReadLine();
+            double weight;
+            double.TryParse(w, out weight);
+
+            double index = weight / (height * height);
+            
+            Console.WriteLine($"Your index is: {index:F2}");
+            
+            Console.ReadKey();
+        }
+    }
+}
