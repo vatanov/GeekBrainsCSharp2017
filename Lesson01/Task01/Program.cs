64add834ba43f7fa54e0a492c2bf25686288211b
Vladimir Atanov <atanov.v@gmail.com>
2018-01-24 21:03:02 +0200

    Lesson 1 Task 1 is done.

--- Lesson01/Task01/Program.cs
+++ Lesson01/Task01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task01
 {
@@ -10,6 +6,26 @@ namespace Task01
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your first name: ");
+            string fName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            string lName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+
+            Console.Write("Enter your height: ");
+            string height = Console.ReadLine();
+
+            Console.Write("Enter your weight: ");
+            string weight = Console.ReadLine();
+
+            Console.WriteLine("First name: " + fName + "Last name: " + lName + "Age: " + age + "Height: " + height + "Weight: " + weight);
+            Console.WriteLine("First name: {0}; Last name: {1}; Age: {2}; Height: {3}; Weight: {4}", fName, lName, age, height, weight);
+            Console.WriteLine($"First name: {fName}; Last name: {lName}; Age: {age}; Height: {height}; Weight: {weight}");
+
+            Console.ReadKey();
         }
     }
 }
