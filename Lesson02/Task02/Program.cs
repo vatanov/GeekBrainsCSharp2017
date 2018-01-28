0d258548a66b4b10848e958589108d5db98aee6c
Vladimir Atanov <atanov.v@gmail.com>
2018-01-28 13:43:11 +0200

    Lesson 2 Task 2 is done.

---
+++ Lesson02/Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter number: ");
+            string s = Console.ReadLine();
+            Int32.TryParse(s, out num);
+
+            int count = NumberCounter(num);
+
+            Console.WriteLine($"Amount of numbers in {num} is {count}");
+
+            Console.ReadKey();
+        }
+
+        static int NumberCounter(int a)
+        {
+            int count = 0;
+
+            while(a > 0)
+            {
+                count++;
+                a = a / 10;
+            }
+
+            return count;
+        }
+    }
+}
