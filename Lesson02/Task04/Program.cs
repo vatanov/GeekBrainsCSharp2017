7961ae28b3ce622567f0473a06b1b670b09d1a2a
Vladimir Atanov <atanov.v@gmail.com>
2018-09-13 17:08:06 +0300

    Lesson 2 Task 04 is done.

--- Lesson02/Task04/Program.cs
+++ Lesson02/Task04/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task04
 {
@@ -10,6 +6,53 @@ namespace Task04
     {
         static void Main(string[] args)
         {
+            int effortCounter = 3;
+
+            while (effortCounter > 0)
+            {
+                Console.Write("Enter login: ");
+                string entredLogin = Console.ReadLine();
+                Console.Write("Enter password: ");
+                string entredPassword = Console.ReadLine();
+
+                bool marker = IsUserAuthorized(entredLogin, entredPassword);
+
+                if (marker == true)
+                {
+                    Console.WriteLine("You are authorized!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Login and/or password is incorrect. Please try again.");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.Clear();
+                    effortCounter--;
+                }
+            }
+
+            if(effortCounter == 0)
+            {
+                Console.WriteLine("You entered wrong login/password three times. System is blocked!");
+            }
+            Console.ReadKey();
+
+        }
+
+        private static bool IsUserAuthorized(string entredLogin, string entredPassword)
+        {
+            bool authPassed = false;
+
+            string savedLogin = "root";
+            string savedPassword = "GeekBrains";
+
+
+            if (entredLogin == savedLogin && entredPassword == savedPassword)
+            {
+                authPassed = true;
+            }
+
+            return authPassed;
         }
     }
 }
